72f014715965e6a3c0085f30bef84548e93f2e0f
tjordanv <tylervicari@gmail.com>
2023-01-07 13:01:48 -0500

    removing backend

--- java/src/main/java/com/techelevator/model/UserAlreadyExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.model;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus( value = HttpStatus.NOT_ACCEPTABLE, reason = "User Already Exists.")
-public class UserAlreadyExistsException extends RuntimeException {
-
-}
