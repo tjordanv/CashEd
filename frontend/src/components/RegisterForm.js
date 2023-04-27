50299a1d5226e6fea38417aa94c598db064a169b
tjordanv <tylervicari@gmail.com>
2023-04-27 11:15:42 -0400

    register page designed

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -6,15 +6,15 @@ import Box from "@mui/material/Box"
 import Stack from "@mui/material/Stack"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
-import FormControlLabel from "@mui/material/FormControlLabel"
-import Switch from "@mui/material/Switch"
 
 import classes from "./LoginForm.module.css"
 import { Typography } from "@mui/material"
 
 const RegisterForm = () => {
   const [username, setUsername] = useState("")
+  const [emailAddress, setEmailAddress] = useState("")
   const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
   const [message, setMessage] = useState("")
 
   const navigate = useNavigate()
@@ -22,7 +22,7 @@ const RegisterForm = () => {
   const logInHandler = async (e) => {
     e.preventDefault()
     try {
-      let response = await fetch("http://localhost:8080/auth/login", {
+      let response = await fetch("http://localhost:8080/auth/register", {
         method: "POST",
         mode: "cors",
         headers: {
@@ -30,20 +30,42 @@ const RegisterForm = () => {
         },
         body: JSON.stringify({
           username: username,
-          password: password
+          email: emailAddress,
+          password: password,
+          confirmPassword: confirmPassword,
+          role: "USER"
         })
       })
-      if (response.status === 200) {
+      if (response.status === 201) {
         try {
-          const responseJson = await response.json()
-          localStorage.setItem("jwt", responseJson.accessToken)
-          navigate("/")
+          let response = await fetch("http://localhost:8080/auth/login", {
+            method: "POST",
+            mode: "cors",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              username: username,
+              password: password
+            })
+          })
+          if (response.status === 200) {
+            try {
+              const responseJson = await response.json()
+              localStorage.setItem("jwt", responseJson.accessToken)
+              navigate("/")
+            } catch (error) {
+              setMessage("Error authenticating")
+            }
+          }
         } catch (error) {
-          setMessage("user not found")
+          setMessage("Error")
+          console.log(error)
         }
       }
     } catch (error) {
-      console.log(error)
+      setMessage("Error registering")
+      console.log("error registering")
     }
   }
 
@@ -62,6 +84,15 @@ const RegisterForm = () => {
           />
           <TextField
             variant="outlined"
+            label="Email Address"
+            name="emailAddress"
+            required
+            value={emailAddress}
+            onChange={(e) => setEmailAddress(e.target.value)}
+            className={classes.inputField}
+          />
+          <TextField
+            variant="outlined"
             label="Password"
             type="password"
             name="password"
@@ -70,7 +101,17 @@ const RegisterForm = () => {
             onChange={(e) => setPassword(e.target.value)}
             className={classes.inputField}
           />
-          {message && <Typography>User not found</Typography>}
+          <TextField
+            variant="outlined"
+            label="Confirm Password"
+            type="password"
+            name="confirmPassword"
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className={classes.inputField}
+          />
+          {message && <Typography>{message}</Typography>}
           <Button type="submit" variant="contained" className={classes.button}>
             Create Account
           </Button>
