7554c8c936167dfbbaacdbbd146e8e4f8c168977
tjordanv <tylervicari@gmail.com>
2023-06-16 09:00:35 -0400

    gmailAPI
    added gmailAPI and gmailer service.

--- java/FinApp/src/main/java/com/tjv/FinApp/controller/MailController.java
+++ java/FinApp/src/main/java/com/tjv/FinApp/controller/MailController.java
@@ -1,25 +1,34 @@
 package com.tjv.FinApp.controller;
 
 import com.tjv.FinApp.email.MailService;
+import com.tjv.FinApp.email.GMailer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 public class MailController {
     private final MailService mailService;
+    private final GMailer gMailer;
 
-    @Autowired MailController(MailService mailService) {
+    @Autowired MailController(MailService mailService, GMailer gMailer) {
         this.mailService = mailService;
+        this.gMailer = gMailer;
     }
 
     @GetMapping("/resetPassword")
-    public void resetPassword() {
-        mailService.sendEmail();
+    public void resetPassword() throws Exception {
+        new GMailer().sendMail("A new message", """
+                Dear reader,
+                                
+                Hello world.
+                                
+                Best regards,
+                myself
+                """);
+    }
+        //mailService.sendEmail();
     }
 
-}
