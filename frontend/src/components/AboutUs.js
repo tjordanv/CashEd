855dae5f2a1bfe78d9ad422ec4c6013bd5976791
tjordanv <tylervicari@gmail.com>
2023-08-18 16:47:17 -0400

    about us

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -13,48 +13,54 @@ const AboutUs = () => {
   return (
     <div className={classes.container}>
       <h1>about us layout</h1>
-      <Card variant="outlined" className={classes.bioCard}>
-        <CardContent>
+      <Card className={classes.bioCard}>
+        <CardContent className={classes.cardContent}>
           <div className={classes.rowContainer}>
             <div className={`${classes.backgroundBar} ${classes.top}`}>
               <NavLink
                 to="https://www.linkedin.com/in/tyler-vicari/"
                 target="_blank"
                 rel="noopener noreferrer"
+                className={classes.image}
               >
-                <img
-                  src={linkedin}
-                  alt="linkedin"
-                  className={classes.image}
-                ></img>
+                <img src={linkedin} alt="linkedin" />
               </NavLink>
             </div>
             <div className={classes.overlay}>
-              <Typography>Full Stack Software Engineer</Typography>
+              <Typography variant="h4">Tyler Vicari</Typography>
+              <Typography variant="h5">
+                Creator - Full Stack Software Engineer
+              </Typography>
             </div>
           </div>
           <div className={classes.rowContainer}>
             <div className={`${classes.backgroundBar} ${classes.middle}`}>
               <NavLink
+                className={classes.image}
                 to="https://github.com/tjordanv"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <img src={github} alt="github" className={classes.image}></img>
+                <img src={github} alt="github" />
               </NavLink>
             </div>
             <div className={`${classes.backgroundBar} ${classes.bottom}`}>
-              <img src={email} alt="email" className={classes.image}></img>
+              <img
+                title="TylerVicari@gmail.com"
+                src={email}
+                alt="email"
+                className={classes.image}
+              />
             </div>
             <div className={classes.overlay}>
-              <p>
+              <Typography>
                 In early 2020, my fascination with software development was
                 ignited. Little did I know that this initial spark would lead me
                 down a captivating path of discovery. As I delved deeper into
                 the subject, I found myself immersed in a vast realm of
                 possibilities that had previously been hidden from view.
-              </p>
-              <p>
+              </Typography>
+              <Typography>
                 This newfound passion for software development fundamentally
                 transformed my perspective on technology and its role in our
                 lives. The process of conceptualizing and meticulously crafting
@@ -62,9 +68,14 @@ const AboutUs = () => {
                 me. What was once a mere curiosity has blossomed into a
                 fulfilling routine, where I eagerly anticipate the challenges
                 and rewards that each day's design work brings.
-              </p>
+              </Typography>
             </div>
           </div>
+          <img
+            className={classes.headshot}
+            src={tylerHeadshot}
+            alt="headshot"
+          />
         </CardContent>
       </Card>
     </div>
