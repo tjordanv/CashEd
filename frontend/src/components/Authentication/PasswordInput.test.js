f69556cfeb0c5936ddb3073ee031055d60c14941
tjordanv <tylervicari@gmail.com>
2023-09-05 09:18:11 -0400

    Update PasswordInput.test.js
    extract render function to prevent repeated code

--- frontend/src/components/Authentication/PasswordInput.test.js
+++ frontend/src/components/Authentication/PasswordInput.test.js
@@ -2,10 +2,20 @@ import React from "react"
 import { render, screen, fireEvent } from "@testing-library/react"
 import PasswordInput, { validatePassword } from "./PasswordInput"
 
+const setPasswordHandler = jest.fn()
+const renderComponent = ({ password = "", inputHandler, error }) => {
+  render(
+    <PasswordInput
+      password={password}
+      inputHandler={setPasswordHandler}
+      error={error}
+    />
+  )
+}
+
 describe("PasswordInput", () => {
-  test("renders PasswordInput component", () => {
-    const setPasswordHandler = jest.fn()
-    render(<PasswordInput password="pass" inputHandler={setPasswordHandler} />)
+  test("renders PasswordInput component with correct value", () => {
+    renderComponent({ password: "pass" })
     const passwordInput = screen.getAllByLabelText(/password/i)[0]
 
     expect(passwordInput).toBeInTheDocument()
@@ -13,37 +23,27 @@ describe("PasswordInput", () => {
   })
 
   test("toggles password visibility", () => {
-    render(<PasswordInput password="" inputHandler={() => {}} />)
+    renderComponent({})
     const passwordInput = screen.getAllByLabelText(/password/i)[0]
     const visibilityButton = screen.getByLabelText("show password")
 
     // Password field should be hidden by default
     expect(passwordInput.type).toBe("password")
-
     // Click the visibility button
     fireEvent.click(visibilityButton)
-
     // Password field should become visible
     expect(passwordInput.type).toBe("text")
-
     // Click the visibility button again
     fireEvent.click(visibilityButton)
-
     // Password field should become hidden again
     expect(passwordInput.type).toBe("password")
   })
 
   test("displays error message when isError is true", () => {
-    const errorMessage = "Invalid password"
-    render(
-      <PasswordInput
-        password=""
-        inputHandler={() => {}}
-        error={{ isError: true, message: errorMessage }}
-      />
-    )
+    const error = { isError: true, message: "Invalid password" }
+    renderComponent({ error: error })
 
-    const errorText = screen.getByText(errorMessage)
+    const errorText = screen.getByText(error.message)
     expect(errorText).toBeInTheDocument()
   })
 
