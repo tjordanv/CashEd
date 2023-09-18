d4114ae42ab3650d905029468e851b7f87b99900
tjordanv <67652144+tjordanv@users.noreply.github.com>
2023-09-18 16:20:03 -0400

    landing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,28 +11,35 @@ import Typography from "@mui/material/Typography"
 const Home = () => {
   return (
     <div className={classes.wrapper}>
-      <Box className={classes.tempCont}>
-        <Stack spacing={6}>
-          <div className={classes.stackItem}>
-            <CreditScoreIcon fontSize="large" />
-            <Typography variant="h4">
-              Set and and and achieve budgets
-            </Typography>
-          </div>
-          <div className={classes.stackItem}>
-            <BarChartIcon fontSize="large" />
-            <Typography variant="h4">Set and achieve budgets</Typography>
-          </div>
-          <div className={classes.stackItem}>
-            <CheckCircleIcon fontSize="large" />
-            <Typography variant="h4">Set achieve budgets</Typography>
-          </div>
-          <div className={classes.stackItem}>
-            <CurrencyExchangeIcon fontSize="large" />
-            <Typography variant="h4">Set and and achieve budgets</Typography>
-          </div>
-        </Stack>
-      </Box>
+      <div className={classes.cont}>
+        <div className={classes.titleCont}>
+          <Typography variant="h4">Get to know your money.</Typography>
+          <div></div>
+          <div></div>
+        </div>
+        <Box className={classes.tempCont}>
+          <Stack spacing={6}>
+            <div className={classes.stackItem}>
+              <CreditScoreIcon />
+              <Typography variant="h5">
+                Set and and and achieve budgets
+              </Typography>
+            </div>
+            <div className={classes.stackItem}>
+              <BarChartIcon />
+              <Typography variant="h5">Set and achieve budgets</Typography>
+            </div>
+            <div className={classes.stackItem}>
+              <CheckCircleIcon />
+              <Typography variant="h5">Set achieve budgets</Typography>
+            </div>
+            <div className={classes.stackItem}>
+              <CurrencyExchangeIcon />
+              <Typography variant="h5">Set and and achieve budgets</Typography>
+            </div>
+          </Stack>
+        </Box>
+      </div>
       <img alt="footer" src={footer} className={classes.footer} />
     </div>
   )
