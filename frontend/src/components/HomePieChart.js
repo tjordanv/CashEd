487b03e8f99749668d361600bd08d0ac89116fa5
tjordanv <tylervicari@gmail.com>
2023-09-19 10:14:18 -0400

    home page pie chart

---
+++ frontend/src/components/HomePieChart.js
@@ -0,0 +1,63 @@
+import { PieChart, Pie, Cell } from "recharts"
+import classes from "./HomePieChart.module.css"
+import { useState } from "react"
+import { Typography } from "@mui/material"
+
+const data = [
+  { value: 1, info: "some information to display about the application" },
+  {
+    value: 1,
+    info: "this is also just text that will be displayed when you hover on this section of the pie chart"
+  },
+  {
+    value: 1,
+    info: "this will be some good good stuff about the application and how it helps its users"
+  },
+  {
+    value: 1,
+    info: "one final tidbit of information to display on the home page when the user hovers over the pie chart sections boi"
+  }
+]
+const COLORS = ["#17C3B2", "#227C9D", "#FE6D73", "#FFCB77"]
+
+const HomePieChart = () => {
+  const [info, setInfo] = useState("")
+  const test = (item) => {
+    setInfo(item)
+    console.log(item)
+  }
+  return (
+    <div className={classes.container}>
+      <PieChart width={300} height={350} className={classes.pie}>
+        <Pie
+          data={data}
+          cx={"50%"}
+          cy={"50%"}
+          innerRadius={100}
+          outerRadius={140}
+          paddingAngle={1}
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={COLORS[index]}
+              stroke="none"
+              onMouseEnter={() => test(entry.info)}
+              onMouseLeave={() => setInfo("")}
+            />
+          ))}
+        </Pie>
+      </PieChart>
+      <div
+        className={`${classes.infoContainer} ${
+          info ? "" : classes.infoContainerPlaceholder
+        }`}
+      >
+        <Typography>{info}</Typography>
+      </div>
+    </div>
+  )
+}
+
+export default HomePieChart
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box"
-import classes from "./auth/Auth.module.css"
+import authClasses from "./auth/Auth.module.css"
+import classes from "./Home.module.css"
 import footer from "../assets/AuthFooter.png"
 import Stack from "@mui/material/Stack"
 import BarChartIcon from "@mui/icons-material/BarChart"
@@ -7,40 +8,46 @@ import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange"
 import CheckCircleIcon from "@mui/icons-material/CheckCircle"
 import CreditScoreIcon from "@mui/icons-material/CreditScore"
 import Typography from "@mui/material/Typography"
+import HomePieChart from "../components/HomePieChart"
 
 const Home = () => {
   return (
-    <div className={classes.wrapper}>
+    <div className={authClasses.wrapper}>
       <div className={classes.cont}>
-        <div className={classes.titleCont}>
-          <Typography variant="h4">Get to know your money.</Typography>
-          <div></div>
-          <div></div>
+        <div className={classes.subCont}>
+          <div className={classes.titleCont}>
+            <Typography variant="h4">Get to know your money.</Typography>
+            <div></div>
+            <div></div>
+          </div>
+          <Box className={classes.tempCont}>
+            <Stack spacing={6}>
+              <div className={classes.stackItem}>
+                <CreditScoreIcon />
+                <Typography variant="h5">
+                  Set and and and achieve budgets
+                </Typography>
+              </div>
+              <div className={classes.stackItem}>
+                <BarChartIcon />
+                <Typography variant="h5">Set and achieve budgets</Typography>
+              </div>
+              <div className={classes.stackItem}>
+                <CheckCircleIcon />
+                <Typography variant="h5">Set achieve budgets</Typography>
+              </div>
+              <div className={classes.stackItem}>
+                <CurrencyExchangeIcon />
+                <Typography variant="h5">
+                  Set and and achieve budgets
+                </Typography>
+              </div>
+            </Stack>
+          </Box>
         </div>
-        <Box className={classes.tempCont}>
-          <Stack spacing={6}>
-            <div className={classes.stackItem}>
-              <CreditScoreIcon />
-              <Typography variant="h5">
-                Set and and and achieve budgets
-              </Typography>
-            </div>
-            <div className={classes.stackItem}>
-              <BarChartIcon />
-              <Typography variant="h5">Set and achieve budgets</Typography>
-            </div>
-            <div className={classes.stackItem}>
-              <CheckCircleIcon />
-              <Typography variant="h5">Set achieve budgets</Typography>
-            </div>
-            <div className={classes.stackItem}>
-              <CurrencyExchangeIcon />
-              <Typography variant="h5">Set and and achieve budgets</Typography>
-            </div>
-          </Stack>
-        </Box>
+        <HomePieChart />
       </div>
-      <img alt="footer" src={footer} className={classes.footer} />
+      <img alt="footer" src={footer} className={authClasses.footer} />
     </div>
   )
 }
