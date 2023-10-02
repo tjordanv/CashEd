521daac8e8738d3e41c8f321db0d8efbfd980b1f
tjordanv <tylervicari@gmail.com>
2023-10-02 14:31:33 -0400

    plaid bug fix

--- java/FinApp/src/main/java/com/tjv/FinApp/config/PlaidClientConfig.java
+++ java/FinApp/src/main/java/com/tjv/FinApp/config/PlaidClientConfig.java
@@ -1,3 +1,36 @@
+// Dev version
+
+//package com.tjv.FinApp.config;
+//
+//import com.plaid.client.ApiClient;
+//import com.plaid.client.request.PlaidApi;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.HashMap;
+//
+//@Configuration
+//public class PlaidClientConfig {
+//    @Value("${PLAID_CLIENT_ID}")
+//    private String clientId;
+//    @Value("${PLAID_SECRET}")
+//    private String secret;
+//
+//    @Bean
+//    public PlaidApi plaidClient() {
+//        PlaidApi plaidClient = null;
+//        HashMap<String, String> apiKeys = new HashMap<String, String>();
+//        apiKeys.put("clientId", clientId);
+//        apiKeys.put("secret", secret);
+//        ApiClient apiClient = new ApiClient(apiKeys);
+//        apiClient.setPlaidAdapter(ApiClient.Development); // or equivalent, depending on which environment you're calling into
+//        plaidClient = apiClient.createService(PlaidApi.class);
+//        return plaidClient;
+//    }
+//}
+
+// Sandbox version
 package com.tjv.FinApp.config;
 
 import com.plaid.client.ApiClient;
@@ -14,7 +47,10 @@ public class PlaidClientConfig {
     private String clientId;
     @Value("${PLAID_SECRET}")
     private String secret;
-
+    /**
+     * create been for Plaid client
+     * @return plaidClient
+     */
     @Bean
     public PlaidApi plaidClient() {
         PlaidApi plaidClient = null;
@@ -22,7 +58,7 @@ public class PlaidClientConfig {
         apiKeys.put("clientId", clientId);
         apiKeys.put("secret", secret);
         ApiClient apiClient = new ApiClient(apiKeys);
-        apiClient.setPlaidAdapter(ApiClient.Development); // or equivalent, depending on which environment you're calling into
+        apiClient.setPlaidAdapter(ApiClient.Sandbox);
         plaidClient = apiClient.createService(PlaidApi.class);
         return plaidClient;
     }
--- java/FinApp/src/main/java/com/tjv/FinApp/controller/PlaidController.java
+++ java/FinApp/src/main/java/com/tjv/FinApp/controller/PlaidController.java
@@ -1,37 +1,63 @@
+// Dev version
+
+//package com.tjv.FinApp.controller;
+//import com.plaid.client.model.*;
+//
+//import com.tjv.FinApp.model.LinkTkn;
+//import com.tjv.FinApp.services.PlaidService;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import org.slf4j.Logger;
+//
+//@RestController
+//public class PlaidController {
+//    @Autowired
+//    private PlaidService plaidService;
+//
+//    Logger log = LoggerFactory.getLogger(PlaidController.class);
+//
+//    @CrossOrigin
+//    @GetMapping("/auth/transactions")
+//    public TransactionsGetResponse transactions(@RequestParam String ptkn) throws Exception {
+//        return plaidService.transactions(ptkn);
+//    }
+//    @CrossOrigin
+//    @GetMapping("/accountBalance")
+//    public AccountBalance accountBalance(@RequestParam String ptkn) throws Exception {
+//        return plaidService.accountBalance(ptkn);
+//    }
+//    @CrossOrigin
+//    @GetMapping("/auth/linktkn")
+//    public LinkTkn linkTkn() throws Exception {
+//        LinkTkn tkn = new LinkTkn();
+//        tkn.setLinkTkn(plaidService.pliadToken());
+//        return tkn;
+//    }
+//
+//}
+// Sandbox version
 package com.tjv.FinApp.controller;
-import com.plaid.client.model.*;
 
-import com.tjv.FinApp.model.LinkTkn;
-import com.tjv.FinApp.services.PlaidService;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import org.slf4j.Logger;
+        import com.tjv.FinApp.services.PlaidService;
+        import com.plaid.client.model.AccountBalance;
+        import com.plaid.client.model.TransactionsGetResponse;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PlaidController {
     @Autowired
     private PlaidService plaidService;
-
-    Logger log = LoggerFactory.getLogger(PlaidController.class);
-
-    @CrossOrigin
-    @GetMapping("/auth/transactions")
-    public TransactionsGetResponse transactions(@RequestParam String ptkn) throws Exception {
-        return plaidService.transactions(ptkn);
+    @GetMapping("/transactions")
+    public TransactionsGetResponse transactions() throws Exception {
+        return plaidService.transactions();
     }
-    @CrossOrigin
     @GetMapping("/accountBalance")
-    public AccountBalance accountBalance(@RequestParam String ptkn) throws Exception {
-        return plaidService.accountBalance(ptkn);
-    }
-    @CrossOrigin
-    @GetMapping("/auth/linktkn")
-    public LinkTkn linkTkn() throws Exception {
-        LinkTkn tkn = new LinkTkn();
-        tkn.setLinkTkn(plaidService.pliadToken());
-        return tkn;
+    public AccountBalance accountBalance() throws Exception {
+        return plaidService.accountBalance();
     }
 
 }
