41ab121ee29bd457e7d62a9f5eefffdc32f63088
tjordanv <tylervicari@gmail.com>
2023-10-07 12:39:08 -0400

    Update ContactInfo.java
    added contact info model to server side code

--- java/FinApp/src/main/java/com/tjv/FinApp/model/ContactInfo.java
+++ java/FinApp/src/main/java/com/tjv/FinApp/model/ContactInfo.java
@@ -1,5 +1,67 @@
 package com.tjv.FinApp.model;
 
 public class ContactInfo {
-    private
+    private int id;
+    private boolean isActiveUser;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String message;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isActiveUser() {
+        return isActiveUser;
+    }
+
+    public void setActiveUser(boolean activeUser) {
+        isActiveUser = activeUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
