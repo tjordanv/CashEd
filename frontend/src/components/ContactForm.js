b5428f5a366f4db96ffe766ce855870fdf9aa392
tjordanv <tylervicari@gmail.com>
2023-10-08 10:33:17 -0400

    Update ContactForm.js
    Added the fetch to the frontend to submit the contact us form

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -8,6 +8,7 @@ import MessageInput from "../components/Authentication/MessageInput"
 import FormButton from "../components/Authentication/FormButton"
 import FormControlLabel from "@mui/material/FormControlLabel"
 import Checkbox from "@mui/material/Checkbox"
+import FetchError from "./HelperComponents/FetchError"
 
 const ContactForm = () => {
   const [isActiveUser, setIsActiveUser] = useState(false)
@@ -25,9 +26,50 @@ const ContactForm = () => {
     setUsername("")
   }
 
-  const submitFormHandler = (e) => {
+  const submitFormHandler = async (e) => {
     e.preventDefault()
-    console.log(firstName)
+
+    const body = { message: message, activeUser: isActiveUser }
+    let stringifiedBody
+
+    // set the request body appropriately for a user or non-user
+    if (isActiveUser) {
+      body.username = username
+      stringifiedBody = JSON.stringify(body)
+    } else {
+      body.firstName = firstName
+      body.lastName = lastName
+      body.emailAddress = emailAddress
+
+      stringifiedBody = JSON.stringify(body)
+    }
+
+    try {
+      let response = await fetch("http://localhost:8080/auth/contactUs", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: stringifiedBody
+      })
+      if (!response.ok) {
+        throw await FetchError.fromResponse(response)
+      } else {
+        const responseJson = await response.json()
+        if (responseJson) {
+          console.log("success")
+        } else {
+          console.log("failed to save")
+        }
+      }
+    } catch (error) {
+      if (error instanceof FetchError) {
+        setMessage(error.message)
+      } else {
+        console.log("caught an error")
+      }
+    }
   }
 
   return (
