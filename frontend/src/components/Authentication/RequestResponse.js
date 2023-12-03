61dd312768bba516a05f805e9e54c62a23288c7c
tjordanv <67652144+tjordanv@users.noreply.github.com>
2023-12-03 16:57:42 -0500

    Update RequestResponse.js
    documentation added

--- frontend/src/components/Authentication/RequestResponse.js
+++ frontend/src/components/Authentication/RequestResponse.js
@@ -2,6 +2,10 @@ import Typography from "@mui/material/Typography"
 import classes from "./Auth.module.css"
 import Box from "@mui/material/Box"
 
+/**
+ * The message displayed to users following various authentication requests made by them
+ * @param {string} type the type of request that was made, used to determine the appropriate response
+ */
 const RequestResponse = ({ type }) => {
   let text
   switch (type) {
