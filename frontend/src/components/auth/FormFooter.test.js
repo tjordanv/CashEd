ef538c1d560dab8777efc8f825b0dba769889af9
tjordanv <tylervicari@gmail.com>
2023-12-20 20:00:22 -0500

    Update FormFooter.test.js

--- frontend/src/components/auth/FormFooter.test.js
+++ frontend/src/components/auth/FormFooter.test.js
@@ -1,92 +1,163 @@
 import { render, screen } from "@testing-library/react"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, MemoryRouter } from "react-router-dom"
 import FormFooter from "./FormFooter"
 
+const renderContent = (props) => {
+  render(
+    <MemoryRouter initialEntries={["/"]}>
+      <FormFooter
+        topLink={props.topLink}
+        bottomLink={props.bottomLink}
+        doubleLink={props.doubleLink}
+        tooltip={props.tooltip}
+      />
+    </MemoryRouter>
+  )
+}
+
 describe("FormFooter", () => {
-  it("renders without errors", () => {
-    render(
-      <BrowserRouter>
-        <FormFooter />
-      </BrowserRouter>
-    )
-    // Assert that the component renders without throwing any errors
-    expect(screen.getByTestId("form-footer")).toBeInTheDocument()
-  })
-
-  it("renders a single top link", () => {
-    const topLink = {
-      to: "/register",
-      text: "Create an Account",
-      label: "Need an account?"
-    }
-
-    render(
-      <BrowserRouter>
-        <FormFooter topLink={topLink} />
-      </BrowserRouter>
-    )
-
-    // Assert that the top link is rendered
-    expect(screen.getByText(topLink.label)).toBeInTheDocument()
-    expect(screen.getByText(topLink.text)).toBeInTheDocument()
-    expect(screen.getByTestId("form-footer-top-link")).toHaveAttribute(
-      "href",
-      topLink.to
-    )
-  })
-
-  it("renders a single bottom link", () => {
-    const bottomLink = {
-      to: "/login",
-      text: "Cancel",
-      label: "Go back to login"
-    }
-
-    render(
-      <BrowserRouter>
-        <FormFooter bottomLink={bottomLink} />
-      </BrowserRouter>
-    )
-
-    // Assert that the bottom link is rendered
-    expect(screen.getByText(bottomLink.label)).toBeInTheDocument()
-    expect(screen.getByText(bottomLink.text)).toBeInTheDocument()
-    expect(screen.getByTestId("form-footer-bottom-link")).toHaveAttribute(
-      "href",
-      bottomLink.to
-    )
-  })
-
-  it("renders a double link", () => {
-    const doubleLink = {
-      label: "Having trouble logging in?",
-      firstLink: {
-        to: "/userRecovery/forgotUsername",
-        text: "Forgot Username"
-      },
-      secondLink: {
-        to: "/userRecovery/forgotPassword",
-        text: "Forgot Password"
+  test("renders a single top link without label", () => {
+    const data = {
+      topLink: {
+        to: "/register",
+        text: "Create an Account"
       }
     }
 
-    render(
-      <BrowserRouter>
-        <FormFooter doubleLink={doubleLink} />
-      </BrowserRouter>
-    )
+    renderContent(data)
+    expect(
+      screen.getByRole("link", { name: /Create an Account/i })
+    ).toBeInTheDocument()
+  })
+  test("renders a single top link with label", () => {
+    const data = {
+      topLink: {
+        to: "/register",
+        text: "Create an Account",
+        label: "Need an account?"
+      }
+    }
 
-    // Assert that the double link is rendered
-    expect(screen.getByText(doubleLink.label)).toBeInTheDocument()
-    expect(screen.getByText(doubleLink.firstLink.text)).toBeInTheDocument()
-    expect(screen.getByText(doubleLink.secondLink.text)).toBeInTheDocument()
-    expect(screen.getByTestId("form-footer-first-link")).toHaveAttribute(
-      "href",
-      doubleLink.firstLink.to
-    )
-    expect(screen.getByTestId("form-footer-second-link")).toHaveAttribute(
-      "href",
-      doubleLink.secondLink.to
-    )
+    renderContent(data)
+    expect(screen.getByText(/Need an account?/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole("link", { name: /Create an Account/i })
+    ).toBeInTheDocument()
+  })
+  test("renders a single bottom link without label", () => {
+    const data = {
+      bottomLink: {
+        to: "/register",
+        text: "Create an Account",
+        label: "Need an account?"
+      }
+    }
+
+    renderContent(data)
+    expect(screen.getByText(/Need an account?/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole("link", { name: /Create an Account/i })
+    ).toBeInTheDocument()
+  })
+  test("renders a single bottom top link with label", () => {
+    const data = {
+      bottomLink: {
+        to: "/register",
+        text: "Create an Account",
+        label: "Need an account?"
+      }
+    }
+
+    renderContent(data)
+    expect(screen.getByText(/Need an account?/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole("link", { name: /Create an Account/i })
+    ).toBeInTheDocument()
+  })
+  test("renders top and bottom link without labels", () => {
+    const data = {
+      topLink: {
+        to: "/register",
+        text: "Create an Account"
+      },
+      bottomLink: {
+        to: "/login",
+        text: "Cancel"
+      }
+    }
+    renderContent(data)
+    // Assert that the component renders without throwing any errors
+    expect(
+      screen.getByRole("link", { name: /Create an account?/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /Cancel/i })).toBeInTheDocument()
+  })
+  test("renders top and bottom link with labels", () => {
+    const data = {
+      topLink: {
+        to: "/register",
+        text: "Create an Account",
+        label: "Need an account?"
+      },
+      bottomLink: {
+        to: "/login",
+        text: "Cancel",
+        label: "Already have an account?"
+      }
+    }
+    renderContent(data)
+
+    expect(
+      screen.getByRole("link", { name: /Create an account?/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /Cancel/i })).toBeInTheDocument()
+    expect(screen.getByText(/Need an account?/i)).toBeInTheDocument()
+    expect(screen.getByText(/Already have an account?/i)).toBeInTheDocument()
+  })
+  test("renders top and bottom link with tooltip", () => {
+    const data = {
+      topLink: {
+        to: "/register",
+        text: "Create an Account"
+      },
+      bottomLink: {
+        to: "/login",
+        text: "Cancel"
+      },
+      tooltip: "This is a tooltip"
+    }
+    renderContent(data)
+    // Assert that the component renders without throwing any errors
+    expect(
+      screen.getByRole("link", { name: /Create an account?/i })
+    ).toBeInTheDocument()
+    expect(screen.getByRole("link", { name: /Cancel/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/This is a tooltip/i)).toBeInTheDocument()
+  })
+  test("renders double link with tooltip", () => {
+    const data = {
+      doubleLink: {
+        label: "Having trouble logging in?",
+        firstLink: {
+          to: "/userRecovery/forgotUsername",
+          text: "Forgot Username"
+        },
+        secondLink: {
+          to: "/userRecovery/forgotPassword",
+          text: "Forgot Password"
+        }
+      },
+      tooltip: "This is a tooltip"
+    }
+    renderContent(data)
+    // Assert that the component renders without throwing any errors
+    expect(screen.getByText(/Having trouble logging in?/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole("link", { name: /Forgot Username/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole("link", { name: /Forgot Password/i })
+    ).toBeInTheDocument()
+    expect(screen.getByLabeText(/This is a tooltip/i)).toBeInTheDocument()
   })
 })
