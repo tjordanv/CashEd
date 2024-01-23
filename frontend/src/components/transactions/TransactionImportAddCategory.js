69d55fe13b5b919d2cfe0cc58465440d6c4422ef
tjordanv <tylervicari@gmail.com>
2024-01-23 12:54:38 -0500

    Update TransactionImportAddCategory.js

--- frontend/src/components/transactions/TransactionImportAddCategory.js
+++ frontend/src/components/transactions/TransactionImportAddCategory.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import Box from "@mui/material/Box"
 import TextField from "@mui/material/TextField"
 import Button from "@mui/material/Button"
@@ -18,6 +18,7 @@ import classes from "./TransactionImportAddCategory.module.css"
 import Switch from "@mui/material/Switch"
 import FormControlLabel from "@mui/material/FormControlLabel"
 import Divider from "@mui/material/Divider"
+import Typography from "@mui/material/Typography"
 
 const icon = <CheckBoxOutlineBlankIcon fontSize="small" />
 const checkedIcon = <CheckBoxIcon fontSize="small" />
@@ -27,14 +28,45 @@ const TransactionImportAddCategory = ({ categoryId }) => {
   const [subcategories, setSubcategories] = useState(
     useLoaderData()[categoryId - 1].map((subcategory) => ({
       ...subcategory,
-      label: subcategory.name
+      label: subcategory.name,
+      isChanged: false
     }))
   )
+  const [checkedState, setCheckedState] = useState(true)
+
   //   useEffect(() => {
   //   }, [])
   const addCategories = async () => {}
-  const test = () => {
-    console.log("test")
+  const test = (event) => {
+    event.stopPropagation()
+    console.log(event.target.value)
+  }
+
+  const prevValueRef = useRef()
+  const updateCategoryHandler = (event, newValue) => {
+    const prevValue = prevValueRef.current
+    prevValueRef.current = newValue
+
+    let changedValue
+    if (!prevValue) {
+      changedValue = newValue[0]
+    } else if (prevValue.length < newValue.length) {
+      changedValue = newValue.find((option) => !prevValue.includes(option))
+    } else {
+      changedValue = prevValue.find((option) => !newValue.includes(option))
+    }
+    setSubcategories((prevState) => {
+      return prevState.map((subcategory) => {
+        if (subcategory.id === changedValue.id) {
+          return { ...subcategory, isChanged: !subcategory.isChanged }
+        }
+        return subcategory
+      })
+    })
+  }
+
+  const background = (isActive, isChanged) => {
+    return isChanged && isActive ? "rgba(0, 0, 0, 0.04)" : undefined
   }
   return (
     <>
@@ -48,34 +80,47 @@ const TransactionImportAddCategory = ({ categoryId }) => {
           <DialogContent sx={{ width: "500px" }}>
             <DialogContentText id="alert-dialog-slide-description"></DialogContentText>
             <Autocomplete
-              onChange={test}
+              onChange={(event, value) => updateCategoryHandler(event, value)}
               options={subcategories}
               disableCloseOnSelect
+              disableClearable={true}
               multiple
               getOptionLabel={(option) => option.name}
+              isOptionEqualToValue={(option, value) => option.id === value.id}
               renderOption={(props, option, { selected }) => (
-                <Box>
+                <Box
+                  key={option.id}
+                  sx={{
+                    background: background(option.active, option.isChanged)
+                  }}
+                >
                   <li {...props}>
                     <Checkbox
                       icon={icon}
                       checkedIcon={checkedIcon}
                       style={{ marginRight: 8 }}
                       checked={selected}
+                      // defaultChecked={option.active}
                     />
-                    {option.name} : {option.description}
-                    <FormControlLabel
-                      sx={{ marginLeft: "auto" }}
-                      control={
-                        <Checkbox
-                          defaultChecked
-                          onClick={(event) => {
-                            event.stopPropagation()
-                          }}
-                        />
-                      }
-                      label="Save changes for future use"
-                      labelPlacement="start"
-                    />
+                    <Box sx={{ width: "350px", padding: "5px 0" }}>
+                      <Typography variant="h6">{option.name}</Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        {option.description}
+                      </Typography>
+                    </Box>
+                    <Divider variant="middle" orientation="vertical" />
+                    {option.isChanged && (
+                      <FormControlLabel
+                        sx={{ width: "175px" }}
+                        control={<Switch defaultChecked onClick={test} />}
+                        label={
+                          <Typography variant="caption">
+                            Save changes for future use
+                          </Typography>
+                        }
+                        labelPlacement="start"
+                      />
+                    )}
                   </li>
                   <Divider variant="middle" />
                 </Box>
